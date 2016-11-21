35ccba10e119f0372a311820e790bf43d1a7d677
jonathan dierckens <jonathan.dierckens@persgroep.be>
2016-11-21 12:31:48 +0100

    add automatic deployment with travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 GIT_DEPLOY_REPO=${GIT_DEPLOY_REPO:-$(node -e 'process.stdout.write(require("./package.json").repository)')}
 
+if [ "$TRAVIS" = "true" ]
+then
+  # git need this, on Travis-CI nobody is defined
+  git config --global user.name "Easybird from Travis" && \
+  git config --global user.email "easybird85@gmail.com"
+fi
+
 cd dist && \
 $(npm bin)/rimraf .git
 git init && \
 git add . && \
-git commit -m "Deploy to GitHub Pages" && \
+git commit -m "Deploy static site" && \
 git push --force "${GIT_DEPLOY_REPO}" master:master
-
-# ! #
-# IF YOU USE A USERNAME.GITHUB.IO ROOT DOMAIN, PLEASE READ THE WARNING BELOW
-# ! #
