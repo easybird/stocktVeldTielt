c61c1ee43c5b1227b7ac73c5b122e21ea761b63c
jonathan dierckens <jonathan.dierckens@persgroep.be>
2016-12-02 10:45:01 +0100

    #TG-3 completed initial non-mobile WelcomeHeader

--- src/components/headers/NavBar/index.js
+++ src/components/headers/NavBar/index.js
@@ -11,51 +11,69 @@ const NavBar = (props, { metadata: { pkg } }) => (
       <span className={styles.menuIcon}></span>
       <div className={`${styles.navBarCollapse} ${styles.collapse}`}></div>
     </div>
+    <ul className={styles.mainNav}>
+      <li>
+        <Link
+          className={ styles.link }
+          to="/"
+        >
+          { "Home" }
+        </Link>
+      </li>
+      <li>
+        <Link
+          className={ styles.link }
+          to="/"
+        >
+          { "Locatie" }
+        </Link>
+      </li>
+      <li>
+        <Link
+          className={ styles.link }
+          to="/"
+        >
+          { "Aanbod" }
+        </Link>
+      </li>
+      <li>
+        <Link
+          className={ styles.link }
+          to="/"
+        >
+          { "Verhalen" }
+        </Link>
+      </li>
+      <li>
+        <Link
+          className={ styles.link }
+          to="/"
+        >
+          { "Contact" }
+        </Link>
+      </li>
+      <li>
+        <div className={ styles.navPart2 }>
+          {
+            pkg.facebook &&
+            <a
+              href={ pkg.facebook }
+              className={ styles.link }
+              target="_new"
+            >
+              <Svg svg={ facebookSvg } cleanup/>
+            </a>
+          }
+        </div>
+      </li>
+    </ul>
+
+
     <div className={styles.navbar}>
       <nav className={ styles.nav }>
         <div className={ styles.navPart1 }>
-          <Link
-            className={ styles.link }
-            to="/"
-          >
-            { "Home" }
-          </Link>
-          <Link
-            className={ styles.link }
-            to="/"
-          >
-            { "Locatie" }
-          </Link>
-          <Link
-            className={ styles.link }
-            to="/"
-          >
-            { "Aanbod" }
-          </Link>
-          <Link
-            className={ styles.link }
-            to="/"
-          >
-            { "Verhalen" }
-          </Link>
-          <Link
-            className={ styles.link }
-            to="/"
-          >
-            { "Contact" }
-          </Link>
-          <div className={ styles.navPart2 }>
-            {
-              pkg.facebook &&
-              <a
-                href={ pkg.facebook }
-                className={ styles.link }
-                target="_new"
-              >
-                <Svg svg={ facebookSvg } cleanup/>
-              </a>
-            }
-          </div>
+
+
         </div>
       </nav>
 
--- src/layouts/Stocktveld/index.js
+++ src/layouts/Stocktveld/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Page from '../Page';
+import NavBar from '../../components/headers/NavBar';
 import styles from './index.css';
 import WelcomeHeader from "../../components/headers/WelcomeHeader"
 
@@ -7,6 +8,7 @@ const Stocktveld = (props) => {
   return (
     <div className={ styles.flex }>
       <WelcomeHeader/>
+      <NavBar { ...props }/>
       <Page { ...props }>
       </Page>
     </div>
