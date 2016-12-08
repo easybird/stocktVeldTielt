4a6deb22322e6cbcd03097ca8b36c94d08e4ca9c
jonathan dierckens <jonathan.dierckens@persgroep.be>
2016-12-08 11:13:47 +0100

    #tg-3 update sectionLinks

--- src/components/Content/WelcomeSections/index.js
+++ src/components/Content/WelcomeSections/index.js
@@ -1,29 +1,71 @@
 import React from 'react';
+import { Link } from 'react-router';
 import styles from './index.css';
 
 const WelcomeSections = () => (
   <div className={styles.welcomeSections}>
     <section>
+      <Link to="/">
+        <span className={styles.emptySpan}></span>
+      </Link>
       <h2>Exclusief wonen in het groen met de stad aan je voordeur</h2>
       <div className={styles.longCopy}>Text</div>
-      <button>Ontdek het aanbod</button>
+      <Link className={styles.sectionLink} to="/">
+        Ontdek het aanbod
+      </Link>
     </section>
     <section>
-      <h2>"Quote van iemand"</h2>
-      <div className={styles.longCopy}>Text</div>
-      <button>Lees het verhaal</button>
-    </section>
-    <section>
-      <h2>Uniek klooster met 3 renovatie loften, 20 nieuwbouwwoningen en 14 appartementen</h2>
-      <div className={styles.longCopy}>Text</div>
-      <button>Ontdek het aanbod</button>
-    </section>
-    <section>
-      <h2>Bouw mee aan een warme buurt binnen een mooie stad</h2>
-      <div className={styles.longCopy}>Stocktveld is gelegen in het hartje van Tielt, met zijn locatie vlakbij het
-        station, met scholen binnen handbereik, is het ideaal gelegen.
+      <Link to="/">
+        <span className={styles.emptySpan}></span>
+      </Link>
+      <div className={styles.columns}>
+        <div className={styles.col2md}>
+          <img src="/assets/img/banner/test_foto_1.jpg" alt="Whatever picture"></img>
+        </div>
+        <div className={styles.col2md}>
+          <h2>"Quote van iemand"</h2>
+          <div className={styles.longCopy}>Text</div>
+          <Link className={styles.sectionLink} to="/">
+            Lees het verhaal
+          </Link>
+        </div>
       </div>
-      <button>Ontdek het aanbod</button>
+    </section>
+    <section>
+      <Link to="/">
+        <span className={styles.emptySpan}></span>
+      </Link>
+      <div className={styles.columns}>
+        <div className={styles.col2md}>
+          <h2>Uniek klooster met 3 renovatie loften, 20 nieuwbouwwoningen en 14 appartementen</h2>
+          <div className={styles.longCopy}>Text</div>
+          <Link className={styles.sectionLink} to="/">
+            Ontdek het aanbod
+          </Link>
+        </div>
+        <div className={styles.col2md}>
+          <img src="/assets/img/banner/test_foto_1.jpg" alt="Whatever picture"></img>
+        </div>
+      </div>
+    </section>
+    <section>
+      <Link to="/">
+        <span className={styles.emptySpan}></span>
+      </Link>
+      <div className={styles.columns}>
+        <div className={styles.col2md}>
+          <img src="/assets/img/banner/test_foto_1.jpg" alt="Whatever picture"></img>
+        </div>
+        <div className={styles.col2md}>
+
+          <h2>Bouw mee aan een warme buurt binnen een mooie stad</h2>
+          <div className={styles.longCopy}>Stocktveld is gelegen in het hartje van Tielt, met zijn locatie vlakbij het
+            station, met scholen binnen handbereik, is het ideaal gelegen.
+          </div>
+          <Link className={styles.sectionLink} to="/">
+            Bekijk de locatie
+          </Link></div>
+      </div>
     </section>
   </div>
 );
