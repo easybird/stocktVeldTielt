db7c48f538703d65d60a620650fea1e8c3844010
jonathan dierckens <jonathan.dierckens@persgroep.be>
2017-02-18 16:26:39 +0100

    fix eslint

--- content/assets/js/googleMaps.js
+++ content/assets/js/googleMaps.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars, no-undef */
 function initMap() {
   var uluru = {lat: 50.9895982, lng: 3.3289063};
   var map = new google.maps.Map(document.getElementById('map'), {
