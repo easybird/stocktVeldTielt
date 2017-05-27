00e6b39ebb7dce6702969b3b519f4ea5f2c2ea81
Jonathan Dierckens <jonathan.dierckens@rombit.be>
2017-05-27 14:29:35 +0200

    add new infosessie to website

--- src/layouts/custom/Infosessies/index.js
+++ src/layouts/custom/Infosessies/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import SectionText from '../../../components/Content/SectionText';
-import MailTo from '../../../components/MailTo';
-import ExternalLink from '../../../components/ExternalLink';
+import React from "react";
+import SectionText from "../../../components/Content/SectionText";
+import MailTo from "../../../components/MailTo";
+import ExternalLink from "../../../components/ExternalLink";
 import "./index.css";
 
 const Infosessies = ({ href, linkText }) => (
@@ -10,25 +10,47 @@ const Infosessies = ({ href, linkText }) => (
     href={href}
     linkText={linkText}
   >
-    <div>Op onze infosessies leer je het project ten volle kennen. Je maakt er kennis met de aspecten rond de
+    <div>
+      Op onze infosessies leer je het project ten volle kennen. Je maakt er kennis met de aspecten rond de
       bouw, de prijs, de visie van de huidige bewoners rond het project.
       En dat allemaal door een aantal mensen van de huidige bewoners
-    </div><br/>
-    <div>De eerstvolgende data waarop je lang kan komen:
+    </div><br />
+    <div>
+      De eerstvolgende data waarop je lang kan komen:
       <ul>
-        <li><strong>Momenteel is er geen infosessie gepland wegens de opendeurdag van 20 mei, allen welkom!</strong></li>
+        <li><strong>Maandag 26 juni, om 20u</strong></li>
       </ul>
-      <br/>
+      <br />
       De infosessies vinden steeds plaats op onze site, in het klooster.
       <ul>
         <li>
-          <strong> Blekerijstraat 22,
-            Tielt </strong></li>
+          <strong>
+            {" "}Blekerijstraat 22,
+            Tielt{" "}
+          </strong>
+        </li>
       </ul>
-      <br/>
-      Wil je graag meer weten? Of wil je ons laten weten dat je komt?<br/><br/>
-      Schrijf je in via het <strong><ExternalLink href="http://www.cohousingprojects.be/index.php/interesseformulier" text="inschrijvingsformulier" /></strong> of stuur ons een mailtje: <strong>
-        <MailTo emailAddress="stocktveld@gmail.com" text="stocktveld@gmail.com" subject="Interesse%20in%20de%20infosessie"/>
+      <br />
+      Wil je graag meer weten? Of wil je ons laten weten dat je komt?
+      <br />
+      <br />
+      Schrijf je in via het
+      {" "}
+      <strong>
+        <ExternalLink
+          href="http://www.cohousingprojects.be/index.php/interesseformulier"
+          text="inschrijvingsformulier"
+        />
+      </strong>
+      {" "}
+      of stuur ons een mailtje:
+      {" "}
+      <strong>
+        <MailTo
+          emailAddress="stocktveld@gmail.com"
+          text="stocktveld@gmail.com"
+          subject="Interesse%20in%20de%20infosessie"
+        />
       </strong>
     </div>
   </SectionText>
--- src/layouts/custom/StocktveldContact/index.js
+++ src/layouts/custom/StocktveldContact/index.js
@@ -1,18 +1,18 @@
-import React, { PropTypes, Component } from 'react';
-import NavBar from '../../../components/headers/NavBar';
-import styles from './index.css';
-import ThemeHeader from '../../../components/headers/ThemeHeader';
-import Metadata from '../../../components/head/Metadata';
-import TwoColumnSection from '../../../components/Content/TwoColumnSection';
-import OneColumnSection from '../../../components/Content/OneColumnSection';
-import NewsletterSubscriptionForm from '../../../components/NewsletterSubscriptionForm';
-import SectionImage from '../../../components/Content/SectionImage';
-import SectionText from '../../../components/Content/SectionText';
-import MailTo from '../../../components/MailTo';
-import Infosessies from '../Infosessies';
-import Button from '../../../components/Button';
-import Opendeur from '../../../layouts/custom/Opendeur';
-import { Element } from 'react-scroll';
+import React, { PropTypes, Component } from "react";
+import NavBar from "../../../components/headers/NavBar";
+import styles from "./index.css";
+import ThemeHeader from "../../../components/headers/ThemeHeader";
+import Metadata from "../../../components/head/Metadata";
+import TwoColumnSection from "../../../components/Content/TwoColumnSection";
+import OneColumnSection from "../../../components/Content/OneColumnSection";
+import NewsletterSubscriptionForm
+  from "../../../components/NewsletterSubscriptionForm";
+import SectionImage from "../../../components/Content/SectionImage";
+import SectionText from "../../../components/Content/SectionText";
+import MailTo from "../../../components/MailTo";
+import Infosessies from "../Infosessies";
+import Button from "../../../components/Button";
+import { Element } from "react-scroll";
 
 class StocktveldContact extends Component {
   constructor(props) {
@@ -25,43 +25,46 @@ class StocktveldContact extends Component {
 
   hashLinkScroll() {
     const { hash } = window.location;
-    if (hash !== '') {
+    if (hash !== "") {
       setTimeout(() => {
-        const id = hash.replace('#', '');
+        const id = hash.replace("#", "");
         const element = document.getElementById(id);
-        if (element) element.scrollIntoView({ block: "start", behavior: "instant" });
+        if (element)
+          element.scrollIntoView({ block: "start", behavior: "instant" });
       }, 0);
     }
   }
 
   render() {
     return (
-      <div className={ styles.stocktveld }>
-        <Metadata { ...this.props } />
-        <ThemeHeader
-          height="20vh"
-          hero={this.props.head.hero}
-          hasLogo={true}
-        />
-        <NavBar { ...this.props }/>
+      <div className={styles.stocktveld}>
+        <Metadata {...this.props} />
+        <ThemeHeader height="20vh" hero={this.props.head.hero} hasLogo={true} />
+        <NavBar {...this.props} />
         <div className={styles.welcomeSections}>
           <TwoColumnSection noPadding>
             <div>
-              <SectionText
-                title="Contacteer ons"
-              >
-                <div>We zijn bereikbaar via heel wat kanalen: email, telefoon, persoonlijk contact via onze infosessies,
+              <SectionText title="Contacteer ons">
+                <div>
+                  We zijn bereikbaar via heel wat kanalen: email, telefoon, persoonlijk contact via onze infosessies,
                   facebook of onze nieuwbrief. Kies het kanaal wat jou het beste ligt om kennis met ons te maken.
                 </div>
-                <br/>
-                <div>Email:
-                  <MailTo emailAddress="stocktveld@gmail.com" text=" stocktveld@gmail.com "
-                          subject="Interesse%20in%20stocktveld"/>
+                <br />
+                <div>
+                  Email:
+                  <MailTo
+                    emailAddress="stocktveld@gmail.com"
+                    text=" stocktveld@gmail.com "
+                    subject="Interesse%20in%20stocktveld"
+                  />
                 </div>
                 <div>Telefoonnummer: 09 278 08 43</div>
               </SectionText>
             </div>
-            <SectionImage imageSource="/assets/img/contact/hands-holding-jigsaw.png" imageAlt="contact"/>
+            <SectionImage
+              imageSource="/assets/img/contact/hands-holding-jigsaw.png"
+              imageAlt="contact"
+            />
           </TwoColumnSection>
           <div className={styles.buttons}>
             <Button
@@ -89,21 +92,18 @@ class StocktveldContact extends Component {
               type={Button.TYPE.FULL}
             />
           </div>
-          <hr/>
-          <Opendeur />
-          <hr/>
+          <hr />
           <Element name="infosessies" id="infosessies">
             <OneColumnSection>
               <Infosessies />
             </OneColumnSection>
           </Element>
-          <hr/>
+          <hr />
           <TwoColumnSection>
             <Element name="facebook" id="facebook">
-              <SectionText
-                title="Facebook"
-              >
-                <div>Via onze facebookpagina kun je op de hoogte blijven van het reilen en zeilen binnen het project.
+              <SectionText title="Facebook">
+                <div>
+                  Via onze facebookpagina kun je op de hoogte blijven van het reilen en zeilen binnen het project.
                   Neem gerust een kijkje!
                 </div>
                 <Button
@@ -118,16 +118,14 @@ class StocktveldContact extends Component {
                 title="Nieuwsbrief"
                 text="Wens je graag op de hoogte gehouden te worden van het reilen en zeilen van ons project? Schrijf je dan in voor onze nieuwsbrief."
               />
-              <br/>
+              <br />
               <NewsletterSubscriptionForm />
             </Element>
           </TwoColumnSection>
-          <hr/>
+          <hr />
           <Element name="email" id="eamil">
             <OneColumnSection>
-              <SectionText
-                title="Email"
-              >
+              <SectionText title="Email">
                 <div>
                   Heb je vragen? Wens je graag een individueel gesprek met 1 van ons? Of zou je graag het klooster en de
                   tuin bezoeken?
@@ -135,8 +133,11 @@ class StocktveldContact extends Component {
                 </div>
                 <div>
                   Stuur ons gerust een mail via
-                  <MailTo emailAddress="stocktveld@gmail.com" text="stocktveld@gmail.com"
-                          subject="Interesse%20in%20stocktveld"/>
+                  <MailTo
+                    emailAddress="stocktveld@gmail.com"
+                    text="stocktveld@gmail.com"
+                    subject="Interesse%20in%20stocktveld"
+                  />
                   .
                 </div>
               </SectionText>
@@ -144,10 +145,9 @@ class StocktveldContact extends Component {
           </Element>
         </div>
       </div>
-    )
+    );
   }
 }
-
 
 StocktveldContact.propTypes = {
   children: PropTypes.node,
@@ -156,4 +156,4 @@ StocktveldContact.propTypes = {
   body: PropTypes.string
 };
 
-export default StocktveldContact
+export default StocktveldContact;
