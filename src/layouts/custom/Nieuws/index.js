1682d3ec00ad2dfc1651e5bfd57666ed6a07941d
Jonathan Dierckens <jonathan.dierckens@rombit.be>
2017-05-28 22:42:11 +0200

    review remarks

--- src/layouts/custom/Nieuws/index.js
+++ src/layouts/custom/Nieuws/index.js
@@ -6,19 +6,21 @@ const Opendeur = () => (
   <OneColumnSection>
     <SectionText title="Dumobil beslist: &quot;we bouwen alles, zo snel mogelijk&quot;">
       <div>
-        Wie naar onze opendeur kwam op zaterdag 20 mei, heeft het al gehoord. Dumobil heeft beslist om alles in één keer te bouwen: de verkochte en niet verkochte units.
+        <b>Mei 2017</b>
+        {" "}
+        - Wie naar onze opendeur kwam op zaterdag 20 mei, heeft het al gehoord. Dumobil heeft beslist om alles in één keer te bouwen: de verkochte en niet verkochte units.
+        Dit is supergoed nieuws voor wie er nog aan twijfelde: de bouw gaat door en we hebben een datum!
+        {" "}
       </div> <br />
       <div>
-        Dit is supergoed nieuws voor wie er nog aan twijfelde: de bouw gaat door en we hebben een datum! We denken dat dit wel wat twijfelaars over de schreef kan strekken. Een grond en vergevorderde bouwplannen hadden we al, de groep is in de maak, maar een bouwdatum en daaraan gekoppeld een verhuisdatum hadden we nog niet. Dat laatste stukje onzekerheid is nu van de baan. We kunnen nu vol goesting de laatste rechte lijn inzetten.
+        Een grond en vergevorderde bouwplannen hadden we al, de groep is in de maak, maar een bouwdatum en daaraan gekoppeld een verhuisdatum hadden we nog niet. Dat laatste stukje onzekerheid is nu van de baan. We kunnen nu vol goesting de laatste rechte lijn inzetten.
+        <br />
+        Tegen begin juli finaliseren we de bouwaanvraag. We plannen om tegen eind februari 2018 de bouw te beginnen.
+        Een jaar later zouden we er dus kunnen wonen.
       </div>
       <br />
-      Tegen begin juli finaliseren we de bouwaanvraag. We verwachtten dat dit ervoor zorgt dat we tegen eind februari, begin maart 2018 kunnen beginnen bouwen.
-      Een jaar later zouden we er dus kunnen wonen.
-      <br />
-      <div>
-        Wat betekent dit voor jullie? De geïnteresseerden. Het is nu het moment om te springen. Er is zekerheid, er is een datum.
-      </div><br /><div style={{ textAlign: "left" }}>
-        Nu snel instappen betekent:
+      <div style={{ textAlign: "left" }}>
+        Wat betekent dit voor jullie, de geïnteresseerden. Het is nu het moment om te springen. Er is zekerheid, er is een datum. Instappen voor aanvang van de bouw betekent:
         <ul
           style={{
             listStyleType: "disc",
@@ -29,7 +31,7 @@ const Opendeur = () => (
         >
           <li>Nog keuze van units (er is nog +/- 50% vrij). </li>
           <li>
-            Mogelijks een korting: Dumobil beloofde een korting voor de huizen die reeds verkocht zijn voor de start van de bouw. Hoe meer er verkocht zijn, hoe groter de korting.
+            Een korting: Dumobil beloofde een korting voor de huizen die reeds verkocht zijn voor de start van de bouw. Hoe meer er verkocht zijn, hoe groter de korting.
           </li>
           <li>
             Nog veel inzeggenschap in de plannen rond de tuin en de inrichting van het paviljoen.
