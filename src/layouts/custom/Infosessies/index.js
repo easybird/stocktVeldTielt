eec89fafa6e7c06e78b47ecfc05cba9f4e6d537c
Jonathan Dierckens <jonathan.dierckens@inthepocket.mobi>
2017-09-05 15:08:30 +0200

    update infosessies

--- src/layouts/custom/Infosessies/index.js
+++ src/layouts/custom/Infosessies/index.js
@@ -4,50 +4,51 @@ import MailTo from "../../../components/MailTo";
 import ExternalLink from "../../../components/ExternalLink";
 import "./index.css";
 
-const Infosessies = ({ href, linkText }) => (
+const Infosessies = ({ href, linkText }) =>
   <SectionText
     title="Welkom op onze infosessies"
     href={href}
     linkText={linkText}
   >
     <div>
-      Op onze infosessies leer je het project ten volle kennen. Je maakt er kennis met de aspecten rond de
-      bouw, de prijs, de visie van de huidige bewoners rond het project.
-      En dat allemaal door een aantal mensen van de huidige bewoners
-    </div><br />
+      Op onze infosessies leer je het project ten volle kennen. Je maakt er
+      kennis met de aspecten rond de bouw, de prijs, de visie van de huidige
+      bewoners rond het project. En dat allemaal door een aantal mensen van de
+      huidige bewoners
+    </div>
+    <br />
     <div>
       De eerstvolgende data waarop je lang kan komen:
       <ul>
-        <li><strong>Zondag 3 september, om 10u</strong></li>
-        <li><strong>Dinsdag 3 oktober, om 20u</strong></li>
-        <li><strong>Zondag 12 november, om 10u</strong></li>
-        <li><strong>Maandag 11 december, om 19u30</strong></li>
+        <li>
+          <strong>Dinsdag 3 oktober, om 20u</strong>
+        </li>
+        <li>
+          <strong>Zondag 12 november, om 10u</strong>
+        </li>
+        <li>
+          <strong>Maandag 11 december, om 19u30</strong>
+        </li>
       </ul>
       <br />
       De infosessies vinden steeds plaats op onze site, in het klooster.
       <ul>
         <li>
-          <strong>
-            {" "}Blekerijstraat 22,
-            Tielt{" "}
-          </strong>
+          <strong> Blekerijstraat 22, Tielt </strong>
         </li>
       </ul>
       <br />
       Wil je graag meer weten? Of wil je ons laten weten dat je komt?
       <br />
       <br />
-      Schrijf je in via het
-      {" "}
+      Schrijf je in via het{" "}
       <strong>
         <ExternalLink
           href="http://www.cohousingprojects.be/index.php/interesseformulier"
           text="inschrijvingsformulier"
         />
-      </strong>
-      {" "}
-      of stuur ons een mailtje:
-      {" "}
+      </strong>{" "}
+      of stuur ons een mailtje:{" "}
       <strong>
         <MailTo
           emailAddress="stocktveld@gmail.com"
@@ -56,8 +57,7 @@ const Infosessies = ({ href, linkText }) => (
         />
       </strong>
     </div>
-  </SectionText>
-);
+  </SectionText>;
 
 Infosessies.propTypes = {
   href: React.PropTypes.string,
